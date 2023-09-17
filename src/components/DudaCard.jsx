ac9d1c4b39fe79baeac37e74f9a58b98eb43bb31
David Kressin <david@garciagonzales.com>
2023-09-17 07:07:40 -0300

    Debo crear la API a consumir

--- src/components/DudaCard.jsx
+++ src/components/DudaCard.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 
-export const DudaCard = () => {
+export const DudaCard = ({username="", header="Header Duda", description="",}) => {
   return (
-    <div className='w-[80%] h-[100px] bg-slate-600 p-3 flex flex-row text-center rounded-full'>
+    <div className='w-[80%] min-h-[100px] bg-slate-600 m-4 p-3 flex flex-row text-center rounded-full'>
         <div className="w-1/4">
             <div className='bg-white h-[50px] w-[50px] rounded-full 
                 border-solid border-black border-2 mx-auto mt-1 overflow-hidden'>
                 <img className='w-[100%]' src='https://media.istockphoto.com/id/1255163297/vector/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-sign-logo-black-silhouette.jpg?s=612x612&w=0&k=20&c=p6azyhUBIcWx6-aXVRPUTveaVqbTA2bNXpBoGQjEB68='/>
             </div>
-            <p><a href="#" target="_blank" rel="noopener noreferrer">NickName</a></p>
+            <p><a href="#" target="_blank" rel="noopener noreferrer">{username}</a></p>
         </div>
 
 
         <div className="w-1/2 text-xs text-justify">
-            Omnis, dolor, odio iure rerum facilis provident tempore deserunt excepturi culpa voluptate exercitationem quasi iste error eligendi vel sed illum quae perspiciatis.
+            <p className='font-bold'>
+              {header}
+            </p>
+
+            {description}
         </div>
 
 
         <div className="w-1/4 flex flex-col justify-center items-center cursor-pointer">
           <button className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-700">
-            Ver Aportes
+            Responder
           </button>
         </div>
 
--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { DudaCard } from '../components/DudaCard'
 
 export const FeedPage = () => {
-
+  const dudasPublicadasArray = [
+    {id:1, username:"David", description:"Omnis, dolor, odio iure rerum facilis provident tempore deserunt excepturi culpa voluptate exercitationem quasi iste error eligendi vel sed illum quae perspiciatis."},
+    {id:2,username:"Fernando", description:"Omnis, dolor, odio iure rerum facilis provident tempore deserunt excepturi culpa voluptate exercitationem quasi iste error eligendi vel sed illum quae perspiciatis."},
+    {id:3,username:"Danilo", description:"Omnis, dolor, odio iure rerum facilis provident tempore deserunt excepturi culpa voluptate exercitationem quasi iste error eligendi vel sed illum quae perspiciatis."},
+    {id:4,username:"Federico", description:"Omnis, dolor, odio iure rerum facilis provident tempore deserunt excepturi culpa voluptate exercitationem quasi iste error eligendi vel sed illum quae perspiciatis."},
+    {id:5,username:"Daniel", description:"Omnis, dolor, odio iure rerum facilis provident tempore deserunt excepturi culpa voluptate exercitationem quasi iste error eligendi vel sed illum quae perspiciatis."},
+    {id:6,username:"Fernanda", description:"Omnis, dolor, odio iure rerum facilis provident tempore deserunt excepturi culpa voluptate exercitationem quasi iste error eligendi vel sed illum quae perspiciatis."},
+    {id:7,username:"Soledad", description:"Omnis, dolor, odio iure rerum facilis provident tempore deserunt excepturi culpa voluptate exercitationem quasi iste error eligendi vel sed illum quae perspiciatis."},
+    {id:8,username:"Maite", description:"Omnis, dolor, odio iure rerum facilis provident tempore deserunt excepturi culpa voluptate exercitationem quasi iste error eligendi vel sed illum quae perspiciatis."},
+  ]
   
 
   return (
@@ -10,7 +19,13 @@ export const FeedPage = () => {
         <div className="">
             <span className='text-basis font-bold'>Dudas Publicadas</span>
         </div>
-        <DudaCard/>
+        {
+          dudasPublicadasArray.map(duda=>{
+            return(
+              <DudaCard key={duda.id} username={duda.username} description={duda.description}/>
+            )
+          })
+        }
 
     </div>
   )
